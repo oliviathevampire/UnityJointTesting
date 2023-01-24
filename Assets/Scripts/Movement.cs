319c3e32da03b2bce652666a9f3fc07b71b10e6c
OliviaTheVampire <sindrefagerheim53@gmail.com>
2023-01-24 10:03:15 +0100

    Added movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Movement : MonoBehaviour {
+    private Rigidbody2D _rb;
+
+    public float speed;
+
+    private void Start() {
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        if (Input.GetKey(KeyCode.A))
+            _rb.velocity = Vector2.left * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            _rb.velocity = Vector2.right * speed * Time.deltaTime;
     }
 }
